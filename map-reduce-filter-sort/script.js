c1aaed6a9fa77d12d03efac1f1794854c6169d69
Jan Redmann <janredmann1@gmail.com>
2020-06-05 15:22:58 +0200

    add filter, reverse and sort

--- map-reduce-filter-sort/script.js
+++ map-reduce-filter-sort/script.js
@@ -1,4 +1,4 @@
-// map
+// map -> calls the callback function on every item of an array and returns a new array
 
 const names = ['john', 'paul', 'ringo', 'george'];
 
@@ -60,7 +60,9 @@ const studentsWithTotalPoints = students.map(function (student) {
     }
 });
 
-// reduce 
+// reduce -> reduces the array that it is called on to one value, we can either provide an initial value as a 2nd
+// parameter after the callback or start with the first item as the initial value for the accumulator
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
 
 // const numbers = [2, 4, 6, 8];
 
@@ -137,3 +139,87 @@ const numbers = [1, 2, 3, 4, 2, 2, 5];
 const evenNumbers = numbers.filter(function (number) {
     return number === 2;
 });
+
+function gooseFilter(birds) {
+    var geese = ["African", "Roman Tufted"];
+    const filteredBirds = birds.filter(function (bird) {
+        return !geese.includes(bird);
+    });
+
+    return filteredBirds;
+};
+
+// https://www.codewars.com/kata/57ee4a67108d3fd9eb0000e7
+
+gooseFilter(["Mallard", "Hook Bill", "African"])
+// ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]);
+
+const places = [
+    {
+        title: "Awesome Suite 20' away from la Rambla",
+        price: 200,
+        type: "Private Room",
+        pool: true,
+        garage: false
+    },
+    {
+        title: "Private apartment",
+        price: 190,
+        type: "Entire Place",
+        pool: true,
+        garage: true
+    },
+    {
+        title: "Apartment with awesome views",
+        price: 400,
+        type: "Entire Place",
+        pool: false,
+        garage: false
+    },
+    {
+        title: "Apartment in la Rambla",
+        price: 150,
+        type: "Private Room",
+        pool: false,
+        garage: true
+    },
+    {
+        title: "Comfortable place in Barcelona´s center",
+        price: 390,
+        type: "Entire place",
+        pool: true,
+        garage: true
+    }
+];
+
+// filter for all the places that have a pool - all the places with a pool should be filtered
+
+// with map
+const placesWithPool = places.filter(function (place) {
+    return place.pool === 'yes';
+}).map(function (place) {
+    return {
+        title: place.title
+    }
+});
+
+// reverse - reverses an array
+
+const word = 'Pie Pie CarPierot Pie';
+
+word.split('').reverse().join('');
+
+// sort() sorts the array 'in place' (using no auxiliary data structure) and returns a new array
+
+const numbers = [22, 23, 99, 68, 1, 0, 9, 112, 223, 64, 18];
+
+numbers.sort(function (a, b) {
+    // this sorts ascending:
+    // if (a < b) return - 1; // if true a is put to the left of the array
+    // if (a === b) return 0; // a is not moved
+    // if (a > b) return 1; // if true a is put to the right of the array
+    return b - a; // sort descending
+    // return a - b; // sort ascending
+});
+
+
